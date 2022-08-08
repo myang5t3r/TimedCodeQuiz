df5ddfed8fa46dd4a80581a3bc80d7687c0b2884
myang5t3r <matthew.yang@hunterdouglas.com>
2022-08-07 20:05:47 -0600

    added start card to start quiz, button added to launch questions

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -17,3 +17,25 @@ function setTime() {
 
 // setTime()
 
+// Lets code the question and answers 
+var myArry = [
+    {question: "What operator can you use to find the data type of a variable?",
+    answer1:["object.property", false],
+    answer2:["this.object", false],
+    answer3:["typeof", true],
+    answer4:["propertyKey", false]
+    },
+    {question: "What does the === operator do in JS?",
+    answer1:["compares two variables are equal", false],
+    answer2:["compares two variables and the data type and equal", true],
+    answer3:["compares two numbers are equal", false],
+    answer4:["compares two string are equal", false]
+    },
+    {question: "In JS what method is used to get an element from an html document ",
+    answer1:["setInterval()", false],
+    answer2:["appendChild()", false],
+    answer3:["getElementById()", true],
+    answer4:["setAttribute()", false]
+    }
+]
+
