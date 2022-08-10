ecd09a628e20891576a1364262895c8f1f47d257
myang5t3r <matthew.yang@hunterdouglas.com>
2022-08-09 21:43:26 -0600

    Createded end card, added count function, added more questions, styled to my liking #Stugle Stugle but geting it done

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -8,24 +8,26 @@ var ansBtn = document.getElementsByClassName("ansBtn");
 var footTxt1 = document.querySelector(".foot_txt1");
 var footTxt2 = document.querySelector(".foot_txt2");
 var timeEl = document.querySelector(".timer")
+var endCard = document.querySelector(".end_card")
 
 // global variables
 var finalScore = 0;
 var timeLeft = 60;
-var qIndex = 0;
-var popupTime = 1;  
+var popupTime = 1;
+var qIndex = 0;  
 
 // Lets place the question and answers into an object
-var myArry = [
+var myArray = [
     {question: "What operator can you use to find the data type of a variable?",
     guessItem:["object.property", "this.object","typeof","propertyKey"],
     answer:"typeof",
     guess:""
     },
     {question: "What does the === operator do in JS?",
-    guessItem:["compares two variables are equal", "compares two variables and the data type and equal",
-    "compares two numbers are equal","compares two string are equal"],
-    answer:"compares two variables and the data type and equal",
+    guessItem:["Compares two variables are equal", 
+    "Compares two variables and the data type and equal",
+    "Compares two numbers are equal","Compares two string are equal"],
+    answer:"Compares two variables and the data type and equal",
     guess:""
     },
     {question: "In JS what method is used to get an element from an html document ",
@@ -44,9 +46,9 @@ var myArry = [
     guess:""
     },
     {question: "What are Higher Order Functions?",
-    guessItem:["Fuctions that operate on other functions", 
+    guessItem:["Functions that operate on other functions", 
     "A function that takes another function as an argument",
-    "a function that returns another function",
+    "A function that returns another function",
     "All the above"],
     answer:"All the above",
     guess:""
@@ -54,9 +56,33 @@ var myArry = [
     {question: "What is recursion?",
     guessItem:["A recurring error in your code", 
     "A function that calls itself directly repeatedly until you get your desire result",
-    "a function that returns another function",
+    "A function that returns another function",
     "All the above"],
     answer:"A function that calls itself directly repeatedly until you get your desire result",
+    guess:""
+    },
+    {question: "What is hoisting in javascript?",
+    guessItem:["does not include variable initializations", 
+    "When variable and function declarations are moved to the top",
+    "Can occur in both local and global scope",
+    "All the above"],
+    answer:"All the above",
+    guess:""
+    },
+    {question: "What are object prototypes in javascript?",
+    guessItem:["A blueprint of an object", 
+    "Javascript inherited properties ",
+    "Allows us to use properties and methods on an object even if properties and methods do not exist on the current object",
+    "All the above"],
+    answer:"All the above",
+    guess:""
+    },
+    {question: "What are generator functions javascript?",
+    guessItem:["A blueprint of an function", 
+    "A special function that can be stopped midway and then continue from where they had stopped",
+    "A function to import a class",
+    "Are not in Javascript"],
+    answer:"A special function that can be stopped midway and then continue from where they had stopped",
     guess:""
     }
 ];
@@ -76,9 +102,9 @@ function setTime() {
 // Let create a function that loads the questions and answers
 // insert questions and answers to buttons from object
 function loadQnA(qIndex) {
-    qCard.textContent = myArry[qIndex].question;
+    qCard.textContent = myArray[qIndex].question;
     for (let i = 0 ; i < ansBtn.length; i++){
-        ansBtn[i].textContent = myArry[qIndex].guessItem[i];
+        ansBtn[i].textContent = myArray[qIndex].guessItem[i];
         }   
     }
 
@@ -93,37 +119,69 @@ function popResults(x) {
         popupTime--;
         if(popupTime === 0) {
             footTxt1.setAttribute("style", "display:none;");
-            footTxt1.setAttribute("style", "display:none;");  
+            footTxt2.setAttribute("style", "display:none;");  
             clearInterval(popupTime)
         }
     }, 100)
-}
+};
+
+// Function for counter variable 
+function count(qIndex){
+    if(qIndex === myArray.length){
+        alert("Done")
+    }else{
+        console.log(qIndex)
+    }
+
+};
 
 // Event Handlers 
 // Start Quiz Handler
 cardSwitcher.addEventListener("click", function(){
-    loadQnA(qIndex)
+    loadQnA(0);
     startCard.setAttribute("style", "display:none;")
     qCard.setAttribute("style", "display:flex;")
     ansCard.setAttribute("style", "display:flex;")   
 }) 
 
-// Event handler selecting a answer
+// Event handler selecting a answer and 
+
 for (let i = 0 ; i < ansBtn.length; i++){
     ansBtn[i].addEventListener("click", function(event){
-        var element = event.target
-        var txt = element.textContent
-        myArry[qIndex].guess = txt
-        console.log( myArry[qIndex].guess)
-        if (myArry[qIndex].guess === myArry[qIndex].answer) {
+        var element = event.target;
+        var txt = element.textContent;
+        myArray[qIndex].guess = txt;
+        // Add if count < length of myArray,length
+        if (myArray[qIndex].guess === myArray[qIndex].answer) {
             qIndex++
             finalScore++;
             popResults(true)
+            count(qIndex)
             loadQnA(qIndex)
         }else{
             qIndex++
             popResults(false)
+            count(qIndex)
             loadQnA(qIndex)
-        }
-    }
-)}
+            }
+        }    
+)};
+
+// for (let i = 0 ; i < ansBtn.length; i++){
+//     ansBtn[i].addEventListener("click", function(event){
+//         var element = event.target
+//         var txt = element.textContent
+//         myArry[qIndex].guess = txt
+//         console.log( myArry[qIndex].guess)
+//         if (myArry[qIndex].guess === myArry[qIndex].answer) {
+//             qIndex++
+//             finalScore++;
+//             popResults(true)
+//             loadQnA(qIndex)
+//         }else{
+//             qIndex++
+//             popResults(false)
+//             loadQnA(qIndex)
+//         }
+//     }
+// )}
